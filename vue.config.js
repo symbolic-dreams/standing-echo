82857580f4705d9afdced29431b0e167964fd40d
Michael Haufe <tno@thenewobjective.com>
2020-09-13 02:32:03 -0500

    updated manifest and icons

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
+    //productionTip: false,
     publicPath: process.env.NODE_ENV === 'production'
         ? '/standing-echo/'
-        : '/'
+        : '/',
+    pwa: {
+        manifestPath: 'manifest.webmanifest',
+        name: 'Standing Echo',
+        themeColor: '#0169CE',
+        msTileColor: '#000000',
+        iconPaths: {
+            favicon32: 'img/icons/favicons/favicon-32x32.png',
+            favicon16: 'img/icons/favicons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple/apple-icon-152x152.png',
+            //maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/microsoft/ms-icon-144x144.png'
+        }
+    }
 }
